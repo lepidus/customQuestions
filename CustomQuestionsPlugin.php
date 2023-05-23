437dd18b72201ddd4a4e134635c05e516717b6d4
Thiago Matos <thiago@lepidus.com.br>
2023-05-23 15:22:35 -0400

    Load CustomQuestionGridHandler component
    Issue: documentacao-e-tarefas/scielo#532
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

--- CustomQuestionsPlugin.php
+++ CustomQuestionsPlugin.php
@@ -2,16 +2,23 @@
 
 namespace APP\plugins\generic\customQuestions;
 
+use PKP\plugins\Hook;
+use APP\core\Application;
+use PKP\core\JSONMessage;
+use PKP\linkAction\LinkAction;
 use PKP\plugins\GenericPlugin;
+use APP\template\TemplateManager;
+use PKP\linkAction\request\AjaxModal;
 use Illuminate\Database\Migrations\Migration;
 
 class CustomQuestionsPlugin extends GenericPlugin
 {
-    public function register($category, $path, $mainContextId = null)
+    public function register($category, $path, $mainContextId = null): bool
     {
         $success = parent::register($category, $path);
 
         if ($success && $this->getEnabled()) {
+            Hook::add('LoadComponentHandler', [$this, 'setupGridHandler']);
         }
 
         return $success;
@@ -30,5 +37,60 @@ class CustomQuestionsPlugin extends GenericPlugin
     public function getInstallMigration(): Migration
     {
         return new CustomQuestionsSchemaMigration();
+    }
+
+    public function setupGridHandler(string $hookName, array $params): bool
+    {
+        $component = & $params[0];
+        if ($component == 'plugins.generic.customQuestions.controllers.grid.CustomQuestionGridHandler') {
+            return true;
+        }
+        return false;
+    }
+
+    public function getActions($request, $actionArgs): array
+    {
+        $router = $request->getRouter();
+        return array_merge(
+            $this->getEnabled() ? [
+                new LinkAction(
+                    'settings',
+                    new AjaxModal(
+                        $router->url(
+                            $request,
+                            null,
+                            null,
+                            'manage',
+                            null,
+                            [
+                                'plugin' => $this->getName(),
+                                'category' => $this->getCategory(),
+                                'action' => 'index'
+                            ]
+                        ),
+                        $this->getDisplayName()
+                    ),
+                    __('manager.plugins.settings'),
+                    null
+                )
+            ] : [],
+            parent::getActions($request, $actionArgs)
+        );
+    }
+
+    public function manage($args, $request): JSONMessage
+    {
+        $templateMgr = TemplateManager::getManager($request);
+        $dispatcher = $request->getDispatcher();
+        return $templateMgr->fetchAjax(
+            'customQuestionGridUrlGridContainer',
+            $dispatcher->url(
+                $request,
+                Application::ROUTE_COMPONENT,
+                null,
+                'plugins.generic.customQuestions.controllers.grid.CustomQuestionGridHandler',
+                'fetchGrid'
+            )
+        );
     }
 }
