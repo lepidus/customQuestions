008d4c966fb6187d9214c7ba3498c428cec1c982
Thiago Matos <thiago@lepidus.com.br>
2023-05-24 11:49:57 -0400

    Implements CustomQuestionForm execute function
    Issue: documentacao-e-tarefas/scielo#532
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

--- controllers/grid/form/CustomQuestionForm.php
+++ controllers/grid/form/CustomQuestionForm.php
@@ -56,4 +56,75 @@ class CustomQuestionForm extends Form
     {
         $this->readUserVars(['title', 'description', 'required', 'questionType', 'possibleResponses']);
     }
+
+    public function execute(...$functionArgs)
+    {
+        $customQuestionDAO = DAORegistry::getDAO('CustomQuestionDAO');
+        $request = Application::get()->getRequest();
+
+        if ($this->customQuestionId) {
+            $customQuestion = $customQuestionDAO->getById($this->customQuestionId);
+        } else {
+            $customQuestion = $customQuestionDAO->newDataObject();
+            $customQuestion->setSequence(REALLY_BIG_NUMBER);
+        }
+
+        $customQuestion->setTitle($this->getData('title'), null);
+        $customQuestion->setDescription($this->getData('description'), null);
+        $customQuestion->setRequired($this->getData('required') ? 1 : 0);
+        $customQuestion->setQuestionType($this->getData('questionType'));
+
+        if (in_array($this->getData('questionType'), $customQuestion->getMultipleResponsesQuestionTypes())) {
+            $this->setData('possibleResponsesProcessed', $customQuestion->getPossibleResponses(null));
+            ListbuilderHandler::unpack(
+                $request,
+                $this->getData('possibleResponses'),
+                [$this, 'deleteEntry'],
+                [$this, 'insertEntry'],
+                [$this, 'updateEntry']
+            );
+            $customQuestion->setPossibleResponses($this->getData('possibleResponsesProcessed'), null);
+        } else {
+            $customQuestion->setPossibleResponses(null, null);
+        }
+        if ($customQuestion->getId()) {
+            $customQuestionDAO->deleteSetting($customQuestion->getId(), 'possibleResponses');
+            $customQuestionDAO->updateObject($customQuestion);
+        } else {
+            $this->customQuestionId = $customQuestionDAO->insertObject($customQuestion);
+            $customQuestionDAO->resequenceReviewFormElements();
+        }
+        parent::execute(...$functionArgs);
+        return $this->customQuestionId;
+    }
+
+    public function insertEntry(PKPRequest $request, int $newRowId): bool
+    {
+        $possibleResponsesProcessed = (array) $this->getData('possibleResponsesProcessed');
+        foreach ($newRowId['possibleResponse'] as $key => $value) {
+            $possibleResponsesProcessed[$key][] = $value;
+        }
+        $this->setData('possibleResponsesProcessed', $possibleResponsesProcessed);
+        return true;
+    }
+
+    public function deleteEntry(PKPRequest $request, int $newRowId): bool
+    {
+        $possibleResponsesProcessed = (array) $this->getData('possibleResponsesProcessed');
+        foreach (array_keys($possibleResponsesProcessed) as $locale) {
+            unset($possibleResponsesProcessed[$locale][$rowId - 1]);
+        }
+        $this->setData('possibleResponsesProcessed', $possibleResponsesProcessed);
+        return true;
+    }
+
+    public function updateEntry(PKPRequest $request, int $rowId, int $newRowId): bool
+    {
+        $possibleResponsesProcessed = (array) $this->getData('possibleResponsesProcessed');
+        foreach ($newRowId['possibleResponse'] as $locale => $value) {
+            $possibleResponsesProcessed[$locale][$rowId - 1] = $value;
+        }
+        $this->setData('possibleResponsesProcessed', $possibleResponsesProcessed);
+        return true;
+    }
 }
