94928e136757ce87db54758a955a867c41893478
Thiago Matos <thiago@lepidus.com.br>
2023-05-30 14:32:44 -0400

    Creates Custom Question Response class
    Issue: documentacao-e-tarefas/scielo#534
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lais Reis <lais@lepidus.com.br>

---
+++ classes/customQuestionResponse/CustomQuestionResponse.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace APP\plugins\generic\customQuestions\classes\customQuestionResponse;
+
+class CustomQuestionResponse extends \PKP\core\DataObject
+{
+    public function getSubmissionId()
+    {
+        return $this->getData('submissionId');
+    }
+
+    public function setSubmissionId($submissionId)
+    {
+        $this->setData('submissionId', $submissionId);
+    }
+
+    public function getCustomQuestionId()
+    {
+        return $this->getData('customQuestionId');
+    }
+
+    public function setCustomQuestionId($customQuestionId)
+    {
+        $this->setData('customQuestionId', $customQuestionId);
+    }
+
+    public function getValue()
+    {
+        return $this->getData('value');
+    }
+
+    public function setValue($value)
+    {
+        $this->setData('value', $value);
+    }
+
+    public function getResponseType()
+    {
+        return $this->getData('type');
+    }
+
+    public function setResponseType($type)
+    {
+        $this->setData('type', $type);
+    }
+}
+
+if (!PKP_STRICT_MODE) {
+    class_alias(
+        'APP\plugins\generic\customQuestions\classes\customQuestionResponse\CustomQuestionResponse',
+        '\CustomQuestionResponse'
+    );
+}
