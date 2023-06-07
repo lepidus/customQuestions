3cc9049c39da13322a3654ff67d5f2507c7eb70a
lais <lais@lepidus.com.br>
2023-06-07 14:51:20 -0400

    Create custom question response DAO
    Issue: documentacao-e-tarefas/scielo#534
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lais Reis <lais@lepidus.com.br>

---
+++ classes/customQuestionResponse/DAO.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace APP\plugins\generic\customQuestions\classes\customQuestionResponse;
+
+use Illuminate\Support\Facades\DB;
+use PKP\core\EntityDAO;
+
+class DAO extends EntityDAO
+{
+    public $schema = 'customQuestionResponse';
+    public $table = 'custom_question_responses';
+    public $primaryKeyColumn = 'custom_question_response_id';
+    public $primaryTableColumns = [
+        'id' => 'custom_question_response_id',
+        'submissionId' => 'submission_id',
+        'customQuestionId' => 'custom_question_id',
+        'responseType' => 'response_type',
+        'value' => 'response_value'
+    ];
+
+    public function newDataObject(): CustomQuestionResponse
+    {
+        return app(CustomQuestionResponse::class);
+    }
+
+    public function get(int $id): ?CustomQuestionResponse
+    {
+        $row = DB::table($this->table)
+            ->where($this->primaryKeyColumn, $id)
+            ->first();
+        return $row ? $this->fromRow($row) : null;
+    }
+
+    public function insert(CustomQuestionResponse $customQuestionResponse): int
+    {
+        return parent::_insert($customQuestionResponse);
+    }
+
+    public function update(CustomQuestionResponse $customQuestionResponse): void
+    {
+        parent::_update($customQuestionResponse);
+    }
+
+    public function delete(CustomQuestionResponse $customQuestionResponse): void
+    {
+        parent::_delete($customQuestionResponse);
+    }
+}
