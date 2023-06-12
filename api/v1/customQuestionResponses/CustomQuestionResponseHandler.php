29e1d43a5f91139fa8e115eaee468eef7916b246
Thiago Matos <thiago@lepidus.com.br>
2023-06-12 14:54:24 -0400

    Updates custom question response if it has already been registered
    Issue: documentacao-e-tarefas/scielo#534
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lais Reis <lais@lepidus.com.br>

--- api/v1/customQuestionResponses/CustomQuestionResponseHandler.php
+++ api/v1/customQuestionResponses/CustomQuestionResponseHandler.php
@@ -4,12 +4,12 @@ namespace APP\plugins\generic\customQuestions\api\v1\customQuestionResponses;
 
 use APP\plugins\generic\customQuestions\classes\customQuestion\DAO as CustomQuestionDAO;
 use APP\plugins\generic\customQuestions\classes\customQuestionResponse\DAO as CustomQuestionResponseDAO;
-use PKP\handler\APIHandler;
-use PKP\security\Role;
-use Slim\Http\Request;
 use PKP\core\APIResponse;
+use PKP\handler\APIHandler;
 use PKP\security\authorization\ContextAccessPolicy;
 use PKP\security\authorization\UserRolesRequiredPolicy;
+use PKP\security\Role;
+use Slim\Http\Request;
 
 class CustomQuestionResponseHandler extends APIHandler
 {
@@ -68,12 +68,18 @@ class CustomQuestionResponseHandler extends APIHandler
             $customQuestion = $customQuestionDAO->get($customQuestionId);
 
             $customQuestionResponseDAO = app(CustomQuestionResponseDAO::class);
-            $customQuestionResponse = $customQuestionResponseDAO->newDataObject();
-            $customQuestionResponse->setSubmissionId($submissionId);
-            $customQuestionResponse->setCustomQuestionId($customQuestionId);
+            $customQuestionResponse = $customQuestionResponseDAO->getByCustomQuestionId($customQuestionId, $submissionId);
+
+            if (is_null($customQuestionResponse)) {
+                $customQuestionResponse = $customQuestionResponseDAO->newDataObject();
+                $customQuestionResponse->setSubmissionId($submissionId);
+                $customQuestionResponse->setCustomQuestionId($customQuestionId);
+                $customQuestionResponseDAO->insert($customQuestionResponse);
+            }
+
             $customQuestionResponse->setValue($value);
             $customQuestionResponse->setResponseType($customQuestion->getCustomQuestionResponseType());
-            $customQuestionResponseDAO->insert($customQuestionResponse);
+            $customQuestionResponseDAO->update($customQuestionResponse);
         }
 
         return $response->withJson(['message' => 'ok'], 200);
