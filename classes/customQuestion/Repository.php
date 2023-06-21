f58ab9431d7595c12fd38a00a574e1e307e265c0
Thiago Matos <thiago@lepidus.com.br>
2023-06-21 18:32:57 -0400

    Creates custom question repository
    Issue: documentacao-e-tarefas/scielo#543
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

---
+++ classes/customQuestion/Repository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace APP\plugins\generic\customQuestions\classes\customQuestion;
+
+use APP\core\Request;
+use PKP\services\PKPSchemaService;
+
+class Repository
+{
+    public $dao;
+
+    protected $request;
+
+    protected $schemaService;
+
+    public function __construct(DAO $dao, Request $request, PKPSchemaService $schemaService)
+    {
+        $this->dao = $dao;
+        $this->request = $request;
+        $this->schemaService = $schemaService;
+    }
+
+    public function newDataObject(array $params = []): CustomQuestion
+    {
+        $object = $this->dao->newDataObject();
+        if (!empty($params)) {
+            $object->setAllData($params);
+        }
+        return $object;
+    }
+
+    public function exists(int $id): bool
+    {
+        return $this->dao->exists($id);
+    }
+
+    public function get(int $id): ?CustomQuestion
+    {
+        return $this->dao->get($id);
+    }
+
+    public function add(CustomQuestion $customQuestion): int
+    {
+        $id = $this->dao->insert($customQuestion);
+        return $id;
+    }
+
+    public function edit(CustomQuestion $customQuestion, array $params)
+    {
+        $newCustomQuestion = clone $customQuestion;
+        $newCustomQuestion->setAllData(array_merge($newCustomQuestion->_data, $params));
+
+        $this->dao->update($newCustomQuestion);
+    }
+
+    public function delete(CustomQuestion $customQuestion)
+    {
+        $this->dao->delete($customQuestion);
+    }
+
+    public function getCollector(): Collector
+    {
+        return app(Collector::class);
+    }
+}
