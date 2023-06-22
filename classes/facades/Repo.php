217f1e8f2db01f504c6343e8cc5385abbb4c08b6
Thiago Matos <thiago@lepidus.com.br>
2023-06-22 16:23:58 -0400

    Includes custom question response repository in Repo facade
    Issue: documentacao-e-tarefas/scielo#543
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

--- classes/facades/Repo.php
+++ classes/facades/Repo.php
@@ -3,11 +3,17 @@
 namespace APP\plugins\generic\customQuestions\classes\facades;
 
 use APP\plugins\generic\customQuestions\classes\customQuestion\Repository as CustomQuestionRepository;
+use APP\plugins\generic\customQuestions\classes\customQuestionResponse\Repository as CustomQuestionResponseRepository;
 
 class Repo extends \APP\facades\Repo
 {
     public static function customQuestion(): CustomQuestionRepository
     {
         return app(CustomQuestionRepository::class);
+    }
+
+    public static function customQuestionResponse(): CustomQuestionResponseRepository
+    {
+        return app(CustomQuestionResponseRepository::class);
     }
 }
