657427bb6b050e42a3815ba07eee6fc7c8ff26c0
Lívia Gouvêa <livia@mail.mayfirst.org>
2026-04-09 12:24:17 -0300

    fix: resolve extended text box save issue when question title has a comma

--- classes/components/forms/CustomQuestions.php
+++ classes/components/forms/CustomQuestions.php
@@ -43,7 +43,7 @@ class CustomQuestions extends FormComponent
         $customQuestionResponse = Repo::customQuestionResponse()
             ->getByCustomQuestionId($customQuestion->getId(), $submissionId);
 
-        $fieldName = $this->toKebabCase($customQuestion->getLocalizedTitle()) . '-' . $customQuestion->getId();
+        $fieldName = 'customQuestion-' . $customQuestion->getId();
         $fieldComponents = [
             CustomQuestion::CUSTOM_QUESTION_TYPE_SMALL_TEXT_FIELD => new FieldText(
                 $fieldName,
